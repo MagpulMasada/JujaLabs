204c08a910c9d1a039824903931246f1885fcb3b
MagpulMasada <altravkin@gmail.com>
2018-11-10 17:12:46 +0200

    Новая задача. Зеркалирование массива. + уже реализовано вертикальное зеркалирование на квадратных массивах длиной 0, 1 и 2

---
+++ OOP/WebinarTasks/Mirror.java
@@ -0,0 +1,24 @@
+package ua.com.juja.OOP.WebinarTasks;
+
+public class Mirror {
+    public static void main(String[] args) {
+
+    }
+
+    public void upDown(int[][] array) {
+        if (array.length <= 1) return;
+
+        for (int x = 0; x < array.length; x++) {
+            /*int a1 = array[0][x];
+            int a2 = array[1][x];
+
+            array[1][x] = a1;
+            array[0][x] = a2;*/
+            int temp = array[0][x];
+            array[0][x] = array[1][x];
+            array[1][x] = temp;
+
+        }
+
+    }
+}
---
+++ OOP/WebinarTasks/MirrorTest.java
@@ -0,0 +1,85 @@
+package ua.com.juja.OOP.WebinarTasks;
+
+import org.junit.Test;
+
+import static junit.framework.TestCase.fail;
+import static org.junit.Assert.assertEquals;
+
+public class MirrorTest {
+    @Test
+    public void testUpDown_whenArraySizeIs0(){
+       //given
+        Mirror mirror = new Mirror();
+        int[][] array = new int [0][0];
+
+       //when
+
+        mirror.upDown(array);
+
+       //then
+        assertEquals("", toString(array));
+    }
+
+    @Test
+    public void testUpDown_whenArraySizeIs1(){
+        //given
+        Mirror mirror = new Mirror();
+        int[][] array = new int [1][1];
+
+        array[0][0] = 13;
+
+        //when
+
+        mirror.upDown(array);
+
+        //then
+        assertEquals("13\n", toString(array));
+    }
+
+    @Test
+    public void testUpDown_whenArraySizeIs2(){
+        //given
+        Mirror mirror = new Mirror();
+        int[][] array = new int [2][2];
+
+        //12
+        //34
+        array[0][0] = 1;
+        array[1][0] = 3;
+        array[0][1] = 2;
+        array[1][1] = 4;
+
+        assertEquals(
+                "12\n"+
+                         "34\n", toString(array));
+
+        //when
+
+        mirror.upDown(array);
+
+        //then
+        assertEquals(
+                "34\n"+
+                         "12\n", toString(array));
+    }
+
+    private String toString(int[][] array) {
+        String result= "";
+
+        int size = array.length;
+        for (int y = 0; y < size; y++) {
+            int[] row = array[y];
+            if (row.length != size) {
+                fail("Массив не квадратный");
+            }
+            for (int x = 0; x < size; x++) {
+                result += array[y][x];
+            }
+            result += "\n";
+        }
+        
+        return result;
+    }
+
+    //
+}
